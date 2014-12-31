7b04d270e9389f090d7baff901f0260fe02b6c85
Liang C <liangc@ospray.bluesky>
2014-12-31 11:17:54 -0500

    - start to work on server code.

---
+++ src/com/bluesky/cloudmontain/Main.java
@@ -0,0 +1,22 @@
+package com.bluesky.cloudmontain;
+
+/**
+ * Trunk Center, the repeater for PTTApp, responsible for
+ *  - registration
+ *  - call management
+ *      + call validation
+ *      + call setup
+ *      + call maintainance
+ *      + call teardown
+ *
+ *  Author: Liang C
+ *  Revision:
+ *      - 2014-12-31: initial version
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
