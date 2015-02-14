cbd0bd19abe4cdfe69f9931e3bce39bb2b2c04ba
Liang C <liangc@ospray.bluesky>
2015-02-14 16:06:53 -0500

    - adding details to CP

--- src/com/bluesky/cloudmontain/CallProcessor.java
+++ src/com/bluesky/cloudmontain/CallProcessor.java
@@ -1,6 +1,7 @@
 package com.bluesky.cloudmontain;
 
 import com.bluesky.common.CallInformation;
+import com.bluesky.common.GlobalConstants;
 import com.bluesky.common.NamedTimerTask;
 import com.bluesky.common.OLog;
 import com.bluesky.protocol.CallInit;
@@ -11,6 +12,7 @@ import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.util.EnumMap;
+import java.util.Timer;
 
 /**
  * Created by liangc on 08/02/15.
@@ -23,11 +25,15 @@ public class CallProcessor {
     public CallProcessor(long grp_id, long su_id, final OLog logger){
         TAG = "CP[" + grp_id + "]";
         mLogger = logger;
+        mTimer = new Timer(TAG);
         initializeSM();
         mStateNode.entry();
     }
 
     public void packetReceived(DatagramPacket packet){
+        if(!validatePacket(packet)){
+            return;
+        }
         saveStateContext();
         mStateNode.packetReceived(packet);
         updateStateContext();
@@ -58,11 +64,50 @@ public class CallProcessor {
         mCallInfo.mTargetId = callInit.getTargetId();
     }
 
+    // to validate/authenticate incoming packet, it shall be from current transmitter,
+    // unless current state is hang or idle.
+    private boolean validatePacket(DatagramPacket packet){
+        switch(mState){
+            case INIT:
+            case TXING:
+            {
+                if( packet.getAddress() != mCallInfo.mSenderIpPort.getAddress()
+                    || packet.getPort() != mCallInfo.mSenderIpPort.getPort() )
+                {
+                    mLogger.d(TAG, "state=" + mState + ", unexp sender:"
+                            + packet.getAddress() + ":" + packet.getPort());
+                    return false;
+                }
+            }
+                break;
+            case HANG:
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
     /** forward packet to all group members, except current transmitting SU */
     private void forwardToGrpMembers(DatagramPacket packet){
 
     }
 
+    private NamedTimerTask createTimerTask(){
+        return null;
+    }
+
+    private void rearmFlyWheel(){
+
+    }
+
+    private void sendCallInit(){
+
+    }
+
+    NamedTimerTask mFlywheelTimerTask;
+    Timer mTimer;
     final CallInformation mCallInfo = new CallInformation();
     final OLog mLogger;
 
@@ -101,7 +146,7 @@ public class CallProcessor {
         public void packetReceived(DatagramPacket packet) {
             short protoType = ProtocolBase.peepType(ByteBuffer.wrap(packet.getData()));
             if(protoType == ProtocolBase.PTYPE_CALL_INIT){
-                // TODO: validate? assert should be enough
+                // TODO: assert type is call-init
                 CallInit callInit = (CallInit) ProtocolFactory.getProtocol(packet);
                 recordCallInfo(callInit, packet);
                 forwardToGrpMembers(packet);
@@ -110,73 +155,182 @@ public class CallProcessor {
         }
     }
 
+    /** start call init sequence
+     * - repeat caller's callInit/Data/Term packets
+     * - for 20ms timeout, sync/send callInit on behalf of caller
+     * - for 120ms timeout, fall to call hang to terminate the session
+     *  NOTE: for all following states, we timeout the call session  and return to idle state.
+     *  Right now, time out is 6 packet, i.e. 20ms * 6.
+     */
     private class StateInit extends StateNode{
 
         @Override
         public void entry() {
-            super.entry();
+            mLogger.d(TAG, "entry init");
+            rearmFlyWheel();
+            mTimerTask = createTimerTask();
+            mTimer.schedule(mTimerTask, GlobalConstants.CALL_PACKET_INTERVAL);
         }
 
         @Override
         public void exit() {
-            super.exit();
+            if( mTimerTask != null){
+                mTimerTask.cancel();
+                mTimerTask = null;
+            }
+            mLogger.d(TAG, "exit init");
         }
 
         @Override
         public void timerExpired(NamedTimerTask timerTask) {
-            super.timerExpired(timerTask);
+            if( timerTask == mFlywheelTimerTask ){
+                mLogger.i(TAG, "flywheel times out in " + mState);
+                mState = State.HANG;
+            } else if( timerTask == mTimerTask ) {
+                mLogger.i(TAG, "init: timer exp");
+                sendCallInit();
+                rearmTxTimer();
+            }
         }
 
         @Override
         public void packetReceived(DatagramPacket packet) {
-            super.packetReceived(packet);
+            ProtocolBase proto = ProtocolFactory.getProtocol(packet);
+            short protoType = proto.getType();
+            switch(protoType){
+                case ProtocolBase.PTYPE_CALL_INIT:
+                    forwardToGrpMembers(packet);
+                    rearmTxTimer();
+                    rearmFlyWheel();
+                    break;
+                case ProtocolBase.PTYPE_CALL_DATA:
+                    forwardToGrpMembers(packet);
+                    mState = State.TXING;
+                    break;
+                case ProtocolBase.PTYPE_CALL_TERM:
+                    forwardToGrpMembers(packet);
+                    mState = State.HANG;
+                    break;
+                default:
+                    mLogger.d(TAG, "init: rxed unexp packet:" + proto.toString());
+                    break;
+            }
         }
+
+        /** rearm tx timer per current elapse time
+         *
+         */
+        private void rearmTxTimer(){
+            mTimerTask = createTimerTask();
+            mTimer.schedule(mTimerTask, GlobalConstants.CALL_PACKET_INTERVAL);
+        }
+
+        NamedTimerTask mTimerTask;
     }
 
+    /** transmitting state
+     *  - repeat caller's calldata/callterm
+     *  - for 120ms flywheel timeout, fall to call hang
+     */
     private class StateTxing extends StateNode{
 
         @Override
         public void entry() {
-            super.entry();
+            mLogger.d(TAG, "entry txing");
+            rearmFlyWheel();
         }
 
         @Override
         public void exit() {
-            super.exit();
+            mLogger.d(TAG, "exit txing");
         }
 
         @Override
-        public void timerExpired(NamedTimerTask timerTask) {
-            super.timerExpired(timerTask);
+        public void timerExpired(NamedTimerTask timerTask){
+            if( timerTask == mFlywheelTimerTask ){
+                mLogger.i(TAG, "flywheel times out in " + mState);
+                mState = State.HANG;
+            }
         }
 
         @Override
         public void packetReceived(DatagramPacket packet) {
-            super.packetReceived(packet);
+            ProtocolBase proto = ProtocolFactory.getProtocol(packet);
+            short protoType = proto.getType();
+            switch(protoType){
+                case ProtocolBase.PTYPE_CALL_INIT:
+                case ProtocolBase.PTYPE_CALL_DATA:
+                    forwardToGrpMembers(packet);
+                    rearmFlyWheel();
+                    break;
+                case ProtocolBase.PTYPE_CALL_TERM:
+                    forwardToGrpMembers(packet);
+                    mState = State.HANG;
+                    break;
+                default:
+                    mLogger.d(TAG, "init: rxed unexp packet:" + proto.toString());
+                    break;
+            }
         }
     }
 
+    /** call hang state
+     *  - repeat caller's calldata/callterm
+     *  - for call init, go to call init
+     *  - for call hang timeout, fall to idle
+     *  - for 20ms timeout, sync/send call term
+     */
     private class StateHang extends StateNode {
 
         @Override
         public void entry() {
-            super.entry();
+            mLogger.d(TAG, "entry call hang");
         }
 
         @Override
         public void exit() {
-            super.exit();
+            mLogger.d(TAG, "exit call hang");
         }
 
         @Override
         public void timerExpired(NamedTimerTask timerTask) {
-            super.timerExpired(timerTask);
+            if( timerTask == mFlywheelTimerTask ){
+                mLogger.i(TAG, "flywheel times out in " + mState);
+                mState = State.IDLE;
+                return;
+            } else if( timerTask == mTimerTask ) {
+                mLogger.i(TAG, "init: timer exp");
+                sendCallInit();
+                rearmTxTimer();
+            }
         }
 
         @Override
         public void packetReceived(DatagramPacket packet) {
-            super.packetReceived(packet);
+            ProtocolBase proto = ProtocolFactory.getProtocol(packet);
+            short protoType = proto.getType();
+            switch(protoType){
+                case ProtocolBase.PTYPE_CALL_INIT:
+                    //TODO: validate call sequence, or start a new call
+                    forwardToGrpMembers(packet);
+                    mState =  State.INIT;
+                    break;
+                case ProtocolBase.PTYPE_CALL_DATA:
+                    forwardToGrpMembers(packet);
+                    rearmFlyWheel();
+                    break;
+                case ProtocolBase.PTYPE_CALL_TERM:
+                    forwardToGrpMembers(packet);
+                    rearmTxTimer();
+                    rearmFlyWheel();
+                    break;
+                default:
+                    mLogger.d(TAG, "hang: rxed unexp packet:" + proto.toString());
+                    break;
+            }
         }
+
+        NamedTimerTask mTimerTask;
     }
 
     private void initializeSM(){
